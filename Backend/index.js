fa10bafc2270ea325aa5aa2386ba84c000395237
riteshjha61253 <jha61253@gmail.com>
2025-07-11 21:49:02 +0530

    Fix:

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/user.route.js";
 import dotenv from "dotenv";
-import cors from "cors"
-import path from "path"
+import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socketIo/server.js";
@@ -16,19 +16,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
-  credentials: true}));
+  credentials: true
+}));
 app.use("/user", userRoute);
 app.use("/user/message", messageRoute);
-// -------deployement----
-if (process.env.NODE_ENV === 'production') {
+
+// -------deployment----
+if (process.env.NODE_ENV === "production") {
   const dirPath = path.resolve();
   app.use(express.static(path.join(dirPath, "Frontend", "dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
   });
 }
-
 
 try {
   mongoose
