951d06ce1f1b79c6501508ab47b7049980bae0a0
riteshjha61253 <jha61253@gmail.com>
2025-07-11 22:57:30 +0530

    Now Live Website Link Is Added

--- Backend/socketIo/server.js
+++ Backend/socketIo/server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://chat-application-j0m9.onrender.com",
     methods: ["GET", "POST"],
   },
 });
