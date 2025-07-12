57d346c86caed0a26e40eec55870f12b6c465bf6
riteshjha61253 <jha61253@gmail.com>
2025-07-12 16:24:28 +0530

    Fixing PATH

--- Backend/Frontend/src/config.js
+++ Backend/Frontend/src/config.js
@@ -4,3 +4,8 @@ const BASE_URL = import.meta.env.DEV
   : import.meta.env.VITE_API_URL;
 
 export default BASE_URL;
+console.log("BASE",BASE_URL);
+console.log("This is import",import.meta.env.DEV,import.meta.env.VITE_API_URL,)
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_API_URL);
+
